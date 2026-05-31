5688af8a45c10ec88a7f2943409dbd4dea954aa5
Scott Petrovic <scottpetrovic@gmail.com>
2026-05-30 19:10:35 -0500

    Fix platform detection

--- src/lib/PlatformManager.ts
+++ src/lib/PlatformManager.ts
@@ -1,24 +1,32 @@
+import { StringStreamHandler } from "three/examples/jsm/libs/fflate.module.js";
+
 /**
  * PlatformManager class
  * We have a desktop application built with Tauri. This will help us conditionally show/hide things on the desktop app
    since there is a good chance the desktop app will not have a internet connection
  **/ 
+
+export enum Platform {
+    Web = "Web",
+    Desktop = "Desktop"
+}
+
 export class PlatformManager {
 
   private isTauri: boolean;
-  private platform: 'desktop' | 'web';
+  private platform: Platform;
 
   constructor() {
     this.isTauri = '__TAURI_INTERNALS__' in window;
-    this.platform = this.isTauri ? 'desktop' : 'web';
-    console.log(this.platform ? 'Plastform: Desktop' : 'Platform: Web');
+    this.platform = this.isTauri ? Platform.Desktop : Platform.Web;
+    console.log(`Platform: ${this.platform}`);
   }
 
 
   public init() {
     document.documentElement.dataset.platform = this.platform;
 
-    if (this.platform === 'desktop') {
+    if (this.platform === Platform.Desktop) {
       const link = document.createElement('link');
       link.rel = 'stylesheet';
       link.href = '/desktop.css';
